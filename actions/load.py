4bd4879c871604e6bc7abb7672ee9cf18aabb235
fwilson <fwilson@fwilson.me>
2014-09-06 02:07:17 -0400

    overview

--- actions/load.py
+++ actions/load.py
@@ -1,4 +1,4 @@
 import os
 def get_value():
-    """the one minute load average is %d"""
+    """the one minute load average is %.2f"""
     return os.getloadavg()[0]
--- rest_server.py
+++ rest_server.py
@@ -1,10 +1,20 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 import actions
+import glob
 
-@app.route('/<action>/<arg>/')
-def get_status(action, arg=):
+@app.route('/overview/')
+def get_overview():
+    mods = [i[8:-3] for i in glob.glob("actions/*.py")]
+    values = []
+    for i in mods:
+        func = getattr(__import__("actions.%s" % i), i, None).get_value
+        values.append({"value": func(), "format_string": func.__doc__})
+    return jsonify(value=values)
+
+@app.route('/<action>/')
+def get_status(action):
     value = getattr(__import__("actions.%s" % action), action, None).get_value
     return jsonify(value=value(), format_string=value.__doc__)
 
-app.run()
+app.run(host='0.0.0.0')

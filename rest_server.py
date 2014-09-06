1f17e1c1843054d76faf96e19fae66b79cf0f9b7
fwilson <fwilson@fwilson.me>
2014-09-06 00:48:59 -0400

    minor changes

--- rest_server.py
+++ rest_server.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 import actions
 
-@app.route('/<action>/')
-def get_status(action):
+@app.route('/<action>/<arg>/')
+def get_status(action, arg=):
     value = getattr(__import__("actions.%s" % action), action, None).get_value
     return jsonify(value=value(), format_string=value.__doc__)
 
